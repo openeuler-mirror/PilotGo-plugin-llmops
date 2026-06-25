f8e8e2ec6b6760db5917f067e12b06fe403e31e7
yichao <yichao@kylinos.cn>
2026-06-25 09:39:40 +0800

    (feat)Add agent_orchestration package init
    Change-Id: I2937413bac0010ed567307797894486998d7972c

---
+++ agent/app/agent_orchestration/__init__.py
@@ -0,0 +1,9 @@
+from app.agent_orchestration.builder import build_agent, build_agents
+from app.common.config.agent_config import AgentConfig, list_agent_configs
+
+__all__ = [
+    "AgentConfig",
+    "build_agent",
+    "build_agents",
+    "list_agent_configs",
+]
