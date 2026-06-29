6514a0a39b25c51488fb53e2874eb1a55649ea72
wwanqii <pengwanqi@kylinos.cn>
2026-06-29 07:48:13 +0000

    feat(server): add topology config REST API
    Signed-off-by: wwanqii <pengwanqi@kylinos.cn>
    
    Change-Id: I50c04c62b8e6b1b61a02a7429be4525301c5aa00

--- server/service/topology/topology.go
+++ server/service/topology/topology.go
@@ -1,1 +1,11 @@
 package topology
+
+// TopologyConfigDTO 拓扑配置实体
+type TopologyConfigDTO struct {
+	ID        int64  `json:"id"`
+	ProjectID int    `json:"project_id"`
+	HostID    string `json:"host_id"`
+	Process   string `json:"process"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
